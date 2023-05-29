7432fcbccf23545ea60445045c79f80e538849ae
Nilesh <npawar025@gmail.com>
2023-05-29 18:09:40 +0530

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+        dolorem delectus fugiat architecto officia quisquam ea eligendi
+        doloribus, unde vel laborum tempore assumenda voluptate itaque corrupti,
+        debitis ipsum? Eius, dolores! Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Voluptatum aperiam eligendi ratione alias laborum,
+        facilis, dicta natus distinctio non perspiciatis sunt tenetur at. Quos
+        eveniet maiores, hic nostrum laboriosam a?
+      </p>
+    </section>
+  );
+};
+
+export default About;
