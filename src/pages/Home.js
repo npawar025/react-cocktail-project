8594e73b2749c1d33bc3440071752f9ab7293e49
Nilesh <npawar025@gmail.com>
2023-05-31 09:23:59 +0530

    Initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import CocktailList from "../components/CocktailList";
 import SearchForm from "../components/SearchForm";
-import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
